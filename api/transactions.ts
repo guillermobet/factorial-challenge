a41125031d539bfc60c3fe287ded16d6607bc54f
Guillermo Betancourt <guillermojbetancourt@gmail.com>
2023-08-28 20:58:40 +0200

    IMPORTANT - Fixing deployment for first deliverable

--- api/transactions.ts
+++ api/transactions.ts
@@ -3,34 +3,30 @@ import { cache } from "react";
 
 export const revalidate: number = 10; // revalidate the data at most every ten seconds
 
-const getTotalUsers: () => Promise<number> = cache(async () => {
-  return await prisma.user.count();
-});
+// const getAggregatedTransactionsAmountInLastMonth: () => Promise<number> = cache(
+//   async () => {
+//     const now = new Date();
+//     const thirtyDaysAgoMidnight = new Date(now);
 
-const getAggregatedTransactionsAmountInLastMonth: () => Promise<number> = cache(
-  async () => {
-    const now = new Date();
-    const thirtyDaysAgoMidnight = new Date(now);
+//     thirtyDaysAgoMidnight.setHours(0, 0, 0, 0);
+//     thirtyDaysAgoMidnight.setDate(thirtyDaysAgoMidnight.getDate() - 30);
 
-    thirtyDaysAgoMidnight.setHours(0, 0, 0, 0);
-    thirtyDaysAgoMidnight.setDate(thirtyDaysAgoMidnight.getDate() - 30);
+//     return (
+//       (
+//         await prisma.transaction.aggregate({
+//           _sum: {
+//             amount: true,
+//           },
+//           where: {
+//             createdAt: {
+//               gte: thirtyDaysAgoMidnight, // Today at 00:00 UTC
+//               lt: now, // Today now
+//             },
+//           },
+//         })
+//       )["_sum"].amount?.toNumber() ?? NaN
+//     );
+//   }
+// );
 
-    return (
-      (
-        await prisma.transaction.aggregate({
-          _sum: {
-            amount: true,
-          },
-          where: {
-            createdAt: {
-              gte: thirtyDaysAgoMidnight, // Today at 00:00 UTC
-              lt: now, // Today now
-            },
-          },
-        })
-      )["_sum"].amount?.toNumber() ?? NaN
-    );
-  }
-);
-
-export { getAggregatedTransactionsAmountInLastMonth };
+export {}; //getAggregatedTransactionsAmountInLastMonth };
