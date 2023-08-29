cdbbd8270371c8b839ba650cb5fd0434afa56f5c
Guillermo Betancourt <guillermojbetancourt@gmail.com>
2023-08-29 12:13:06 +0200

    Removing auth call from sign in component

--- app/(sign-in)/[[...sign-in]]/page.tsx
+++ app/(sign-in)/[[...sign-in]]/page.tsx
@@ -1,9 +1,6 @@
-import { SignIn, auth } from "@clerk/nextjs";
-// import type { User } from "@clerk/nextjs/api";
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  const { userId }: { userId: string | null } = auth();
-  // const user: User | null = await currentUser();
   return (
     <div className="flex flex-col justify-center items-center">
       <SignIn />
